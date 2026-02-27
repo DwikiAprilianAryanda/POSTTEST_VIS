787eb8cf177ec476c207cedb2b563813de56452f
DwikiAprilianAryanda <181479443+DwikiAprilianAryanda@users.noreply.github.com>
2026-02-27 10:53:21 +0700

    done

--- POSTTEST_1/Form1.Designer.vb
+++ POSTTEST_1/Form1.Designer.vb
@@ -22,10 +22,99 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Label1 = New Label()
+        Label2 = New Label()
+        LblPredikat = New Label()
+        TxtIpSemester = New TextBox()
+        TxtIpk = New TextBox()
+        BtnTambah = New Button()
+        BtnReset = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(161, 120)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(86, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "IP Semester"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(161, 191)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(89, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "IP Kumulatif"
+        ' 
+        ' LblPredikat
+        ' 
+        LblPredikat.AutoSize = True
+        LblPredikat.Location = New Point(273, 238)
+        LblPredikat.Name = "LblPredikat"
+        LblPredikat.Size = New Size(53, 20)
+        LblPredikat.TabIndex = 2
+        LblPredikat.Text = "Label3"
+        ' 
+        ' TxtIpSemester
+        ' 
+        TxtIpSemester.Location = New Point(273, 120)
+        TxtIpSemester.Name = "TxtIpSemester"
+        TxtIpSemester.Size = New Size(194, 27)
+        TxtIpSemester.TabIndex = 3
+        ' 
+        ' TxtIpk
+        ' 
+        TxtIpk.Location = New Point(273, 191)
+        TxtIpk.Name = "TxtIpk"
+        TxtIpk.ReadOnly = True
+        TxtIpk.Size = New Size(194, 27)
+        TxtIpk.TabIndex = 4
+        ' 
+        ' BtnTambah
+        ' 
+        BtnTambah.Location = New Point(273, 153)
+        BtnTambah.Name = "BtnTambah"
+        BtnTambah.Size = New Size(94, 29)
+        BtnTambah.TabIndex = 5
+        BtnTambah.Text = "Tambah"
+        BtnTambah.UseVisualStyleBackColor = True
+        ' 
+        ' BtnReset
+        ' 
+        BtnReset.Location = New Point(373, 153)
+        BtnReset.Name = "BtnReset"
+        BtnReset.Size = New Size(94, 29)
+        BtnReset.TabIndex = 6
+        BtnReset.Text = "Reset"
+        BtnReset.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(BtnReset)
+        Controls.Add(BtnTambah)
+        Controls.Add(TxtIpk)
+        Controls.Add(TxtIpSemester)
+        Controls.Add(LblPredikat)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
         Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents LblPredikat As Label
+    Friend WithEvents TxtIpSemester As TextBox
+    Friend WithEvents TxtIpk As TextBox
+    Friend WithEvents BtnTambah As Button
+    Friend WithEvents BtnReset As Button
 
 End Class
--- POSTTEST_1/Form1.vb
+++ POSTTEST_1/Form1.vb
@@ -1,3 +1,51 @@
 ﻿Public Class Form1
+    Dim totalIP As Double
+    Dim jumlahSemester As Integer
+
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles BtnTambah.Click
+        Dim ipBaru As Double
+        Dim Keterangan As String
+
+        If Double.TryParse(TxtIpSemester.Text, ipBaru) Then
+
+            If ipBaru < 0 Or ipBaru > 4 Then
+                MessageBox.Show("Nilai IP harus 0 sampai 4!")
+            Else
+                totalIP = totalIP + ipBaru
+                jumlahSemester = jumlahSemester + 1
+                Dim ipk As Double = totalIP / jumlahSemester
+
+                Dim ipkDibulatkan As Double = Math.Round(ipk, 2)
+
+                TxtIpk.Text = ipkDibulatkan
+
+                If ipkDibulatkan >= 3.01 Then
+                    Keterangan = "Sangat Memuaskan"
+                ElseIf ipkDibulatkan >= 2.76 Then
+                    Keterangan = "Memuaskan"
+                ElseIf ipkDibulatkan >= 2.0 Then
+                    Keterangan = "Cukup"
+                Else
+                    Keterangan = "Kurang"
+                End If
+
+                LblPredikat.Text = Keterangan
+
+                TxtIpSemester.Text = ""
+            End If
+
+        Else
+            MessageBox.Show("Input harus angka!")
+        End If
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles BtnReset.Click
+        totalIP = 0
+        jumlahSemester = 0
+
+        TxtIpSemester.Text = ""
+        TxtIpk.Text = ""
+        LblPredikat.Text = ""
+    End Sub
 
 End Class
